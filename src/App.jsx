641879aed3b9b17640e345b2f232a0059d3a0236
Даниил <deadxraver@yandex.ru>
2025-06-01 00:43:08 +0300

    RA BO TA EM

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Всем привет я App.jsx
+				</p>
+				<img src="https://avatars.mds.yandex.net/i?id=16492d525376ea328f1c34917b67f3f3_l-8000733-images-thumbs&n=13"/>
+			</header>
+		</div>
+	);
+}
+
+export default App;
