0446e686a96c167bd7d5ab0cf5b03537fc8b08a6
Даниил <deadxraver@yandex.ru>
2025-06-01 20:49:56 +0300

    картинки наши и иконка

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,13 +5,15 @@ export default function Contact() {
 			<h2>------</h2>
 
 			<h1>Наша команда</h1>
-			<ul>
-				<li><a href="https://github.com/geruniasun">Гера</a></li>
-				<li><a href="https://github.com/p11menova">Катюха</a></li>
-				<li><a href="https://github.com/deadxraver">Данек</a></li>
-				<li><a href="https://github.com/dllnnx">Алена</a></li>
-				<a href="https://github.com/deadxraver/physics-2">Исходный код</a>
-			</ul>
+			<h2><a href="https://github.com/geruniasun">Гера</a></h2>
+				<img src="img_1.png" alt="null"/>
+			<h2><a href="https://github.com/p11menova">Катюха</a></h2>
+				<img src="img_3.png" alt="null"/>
+			<h2><a href="https://github.com/deadxraver">Данек</a></h2>
+				<img src="img.png" alt="null"/>
+			<h2><a href="https://github.com/dllnnx">Алена</a></h2>
+				<img src="img_2.png" alt="null"/>
+			<br/><a href="https://github.com/deadxraver/physics-2">Исходный код</a>
 		</div>
 	);
 }
