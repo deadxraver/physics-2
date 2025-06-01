75bb6c0fc6fb82793f6456ae736ecd8f8bb6970c
dllnnx <alenadenisova528@gmail.com>
2025-06-01 20:32:31 +0300

    add gorbenka

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import NavBar from './components/NavigationBar';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MagneticInduction from "./components/xvost/MagneticInduction";
-import Fluctuations from "./components/gorb/Fluctuations";
 import Contact from "./components/Contact";
 import Main from "./components/introduction/Main";
+import EjsEmbed from "./components/gorb/EjsEmbed";
 
 function App() {
     return (
@@ -13,7 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Main />} />
                     <Route path="/magnetic_induction" element={<MagneticInduction />} />
-                    <Route path="/fluctuations" element={<Fluctuations />} />
+                    <Route path="/fluctuations" element={<EjsEmbed />} />
                     <Route path="/contact" element={<Contact />} />
                     {/* Редирект на главную, если путь не найден */}
                     <Route path="*" element={<Main/>} />
--- src/components/gorb/Fluctuations.jsx
+++
@@ -1,8 +0,0 @@
-export default function Fluctuations() {
-    return (
-        <div>
-            <h2>--------------------</h2>
-            <h1>тут будет лютый вайб кодинг</h1>
-        </div>
-    );
-}
